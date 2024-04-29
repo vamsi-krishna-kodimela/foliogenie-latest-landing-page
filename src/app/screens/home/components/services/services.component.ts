56932226af8eb041dffcbed76e57617b4b207a04
Vamsi Krishna <vamsikrishnakdml@gmail.com>
2024-04-29 23:39:11 +0530

    Update header component styles and services component content

--- src/app/screens/home/components/services/services.component.ts
+++ src/app/screens/home/components/services/services.component.ts
@@ -5,8 +5,34 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.scss'
+  styleUrl: './services.component.scss',
 })
 export class ServicesComponent {
-
+  services: { icon: string; title: string; description: string }[] = [
+    {
+      icon: 'assets/icons/002-ux-design.svg',
+      title: 'UX Design',
+      description: 'We design user experience for your business.',
+    },
+    {
+      icon: 'assets/icons/003-web-development.svg',
+      title: 'Web Development',
+      description: 'We develop websites for your business.',
+    },
+    {
+      icon: 'assets/icons/004-mobile-apps.svg',
+      title: 'Mobile Apps',
+      description: 'We develop mobile apps for your business.',
+    },
+    {
+      icon: 'assets/icons/005-seo.svg',
+      title: 'SEO',
+      description: 'We provide SEO services for your business.',
+    },
+    {
+      icon: 'assets/icons/006-marketing.svg',
+      title: 'Marketing',
+      description: 'We provide marketing services for your business.',
+    },
+  ];
 }
