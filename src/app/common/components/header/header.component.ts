c595414eedd2062a0ce3c79c73399e6e6a312320
Vamsi Krishna Kodimela <55780006+vamsi-krishna-kodimela@users.noreply.github.com>
2024-06-01 06:48:51 +0000

    feat: Update header and hero component styles
    The header component styles have been updated to change the background gradient and color scheme. Additionally, the hero component styles have been modified to adjust the opacity and background size.

--- src/app/common/components/header/header.component.ts
+++ src/app/common/components/header/header.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  menuItems: { label: string; link: string }[] = [
+    {
+      label: 'Home',
+      link: '/',
+    },
+    {
+      label: 'Services',
+      link: '/services',
+    },
+    {
+      label: 'About',
+      link: '/about',
+    },
+    {
+      label: 'Blog',
+      link: '/blog',
+    },
+  ];
 }
